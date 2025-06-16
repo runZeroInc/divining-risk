165b87aa704a63ea09c6d01e9a53ee91d8be2a2a
Tod Beardsley <todb@hugesuccess.org>
2025-06-16 15:15:02 -0500

    Default now to sort by date-desc
    Signed-off-by: Tod Beardsley <todb@hugesuccess.org>

--- epss-wrapper.sh
+++ epss-wrapper.sh
@@ -3,7 +3,7 @@ START_DATE=2025-03-17 ./collect-epss-scores.sh
 # Generate Parquet files
 python3 build-epss-matrix.py
 # Check for significant changes
-python3 detect-significant-changes.py --magnitude 0.50 --days 1
+python3 detect-significant-changes.py --magnitude 0.50 --days 1 --sort date-desc
 # Check if any are already in CISA's KEV
 # Experiment with pinning the cert.
 #    curl -s https://raw.githubusercontent.com/cisagov/kev-data/refs/heads/develop/known_exploited_vulnerabilities.csv \
