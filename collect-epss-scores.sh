2d3cb4c90682cf63618d16b32fd1d9614b4ab292
Tod Beardsley <todb@packetfu.com>
2025-06-25 08:52:30 -0500

    Fix broken EPSS downloads by skipping them. Ignore gdate vs date
    Signed-off-by: Tod Beardsley <todb@packetfu.com>

--- collect-epss-scores.sh
+++ collect-epss-scores.sh
@@ -3,8 +3,10 @@
 # BSD vs Linux date implementation
 if command -v gdate >/dev/null 2>&1; then
     DATE_BIN="gdate"
+    now=`$DATE_BIN`
 else
-    DATE_BIN="date"
+    echo "[$now] !! gdate not found (try brew install coreutils)"
+    # DATE_BIN="date" # This doesn't actually work :(
 fi
 
 # Set sensible defaults
@@ -20,7 +22,6 @@ one_day_after_end=$($DATE_BIN -I -d "$end_date + 1 day")
 while [[ "$current_date" != "$one_day_after_end" ]]; do
     file="epss_scores-$current_date.csv.gz"
     output="./data/epss-csv/$file"
-    now=`$DATE_BIN`
 
     if [[ -f "$output" ]]; then
         echo "[$now] File exists: $file. Skipping."
@@ -34,7 +35,22 @@ while [[ "$current_date" != "$one_day_after_end" ]]; do
         -o "$output" "$url" || {
             echo "[$now] !! Failed to download $file. Continuing..."
         }
+
+        # Sometimes we download too early in the day and catch an error.
+        if [[ -f "$output" ]]; then
+            # Validate file type
+            filetype=$(file -b --mime-type "$output")
+            if [[ "$filetype" != "application/gzip" ]]; then
+                echo "[$now] !! Invalid file type ($filetype) for $file. Deleting..."
+                rm -f "$output"
+            else
+                echo "[$now] File $file looks valid (gzip)."
+            fi
+        else
+            echo "[$now] !! Failed to download $file. File not saved."
+        fi
     fi
+
 
     # Advance the date
     current_date=$($DATE_BIN -I -d "$current_date + 1 day")
